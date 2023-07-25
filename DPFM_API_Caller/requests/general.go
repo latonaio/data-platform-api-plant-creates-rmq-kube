a7247c15a78880f3c228ccb835d26f2e1c3a644d
maimeimow <134258119+maimeimow@users.noreply.github.com>
2023-07-25 13:25:49 +0900

    release

--- DPFM_API_Caller/requests/general.go
+++ DPFM_API_Caller/requests/general.go
@@ -1,26 +1,19 @@
 package requests
 
 type General struct {
-	BusinessPartner      *int    `json:"BusinessPartner"`
+	BusinessPartner      int     `json:"BusinessPartner"`
 	Plant                string  `json:"Plant"`
-	PlantFullName        string  `json:"PlantFullName"`
-	PlantName            *string `json:"PlantName"`
-	Language             *string `json:"Language"`
-	CreationDate         *string `json:"CreationDate"`
-	CreationTime         *string `json:"CreationTime"`
-	LastChangeDate       *string `json:"LastChangeDate"`
-	LastChangeTime       *string `json:"LastChangeTime"`
-	PlantFoundationDate  string  `json:"PlantFoundationDate"`
-	PlantLiquidationDate string  `json:"PlantLiquidationDate"`
-	SearchTerm1          string  `json:"SearchTerm1"`
-	SearchTerm2          string  `json:"SearchTerm2"`
-	PlantDeathDate       string  `json:"PlantDeathDate"`
-	PlantIsBlocked       bool    `json:"PlantIsBlocked"`
-	GroupPlantName1      string  `json:"GroupPlantName1"`
-	GroupPlantName2      string  `json:"GroupPlantName2"`
-	AddressID            int     `json:"AddressID"`
+	PlantName            string  `json:"PlantName"`
+	PlantFullName        *string `json:"PlantFullName"`
+	Language             string  `json:"Language"`
+	PlantFoundationDate  *string `json:"PlantFoundationDate"`
+	PlantLiquidationDate *string `json:"PlantLiquidationDate"`
+	PlantDeathDate       *string `json:"PlantDeathDate"`
+	AddressID            *int    `json:"AddressID"`
 	Country              string  `json:"Country"`
-	TimeZone             string  `json:"TimeZone"`
-	PlantIDByExtSystem   string  `json:"PlantIDByExtSystem"`
-	IsMarkedForDeletion  bool    `json:"IsMarkedForDeletion"`
+	TimeZone             *string `json:"TimeZone"`
+	PlantIDByExtSystem   *string `json:"PlantIDByExtSystem"`
+	CreationDate         string  `json:"CreationDate"`
+	LastChangeDate       string  `json:"LastChangeDate"`
+	IsMarkedForDeletion  *bool   `json:"IsMarkedForDeletion"`
 }
